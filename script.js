5f1b2914b098d82373137d2423668893d7b0598b
mecuboi <matthewd.jusuf@gmail.com>
2022-08-11 22:20:34 +1000

    inputting command boxes

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// var characterLength = [];
+// var lowercase = [];
+// var uppercase = [];
+// var numeric = [];
+// var specialCharacters = [];
+
+
 // Write password to the #password input
+
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  var characterLength = window.prompt("How many characters would you like your password to have? (8 to 128)");
+  
+  if (characterLength < 8 || characterLength > 128) {
+    window.confirm('Invalid character length selected: ' + characterLength);
+    writePassword();
+    return;
+  } else {
+    var lowercase = window.confirm('Would you like to include lowercase?')
+    var uppercase = window.confirm('Would you like to include uppercase?')
+    var numeric = window.confirm('would you like to include numeric?')
+    var specialCharacters = window.confirm('would you like to include special characters?')
+  }
+
+  function generatePassword() {
+    return;
+  }
+
 }
 
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
